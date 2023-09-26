9d0871b2b214a6fe6ec3ef370b26b3435cce01a7
Yevhen Piven <evgeniy13.91a@gmail.com>
2023-09-26 19:05:53 +0200

    lambda without parameters

--- Lambda/src/ua/com/yevhenpiven/Main.java
+++ Lambda/src/ua/com/yevhenpiven/Main.java
@@ -3,33 +3,44 @@ package ua.com.yevhenpiven;
 import java.util.Iterator;
 
 public class Main {
-
+private static double value1=0.25, value2=78.254;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Operation operation=new Operation() {
-	
-	@Override
-	public double getResult(double value1, double value2) {
-		// TODO Auto-generated method stub
-		return value1+value2;
-	}
-};
-System.out.println(operation.getResult(1.02, 2.358));
-System.out.println("---------");
-Operation operationLambda=(value1, value2)->value1+value2;
-System.out.println(operationLambda.getResult(1.45, 14.58));
-System.out.println("---------");
+		Operation operation=()->{
+			value1=100;
+			return value1+value2;
+		};
+		System.out.println(operation.getResult());
+		System.out.println(value1);
+		System.out.println(value2);
+		
+		
+		
+		/*
+		Operation operation = new Operation() {
 
-Printer printer=string->System.out.println(string);
-printer.println("Hello world");
-System.out.println("---------");
-Factorial factorial= value->{
-	int result =1;
-	for (int i = 1; i <= value; i++) {
-		result *=i;
-	}return result;
-};
-System.out.println(factorial.getResult(6));
+			@Override
+			public double getResult(double value1, double value2) {
+				// TODO Auto-generated method stub
+				return value1 + value2;
+			}
+		};
+		System.out.println(operation.getResult(1.02, 2.358));
+		System.out.println("---------");
+		Operation operationLambda = (value1, value2) -> value1 + value2;
+		System.out.println(operationLambda.getResult(1.45, 14.58));
+		System.out.println("---------");
+
+		Printer printer = string -> System.out.println(string);
+		printer.println("Hello world");
+		System.out.println("---------");
+		Factorial factorial = value -> {
+			int result = 1;
+			for (int i = 1; i <= value; i++) {
+				result *= i;
+			}
+			return result;
+		};
+		System.out.println(factorial.getResult(6));*/
 	}
 
 }
--- Lambda/src/ua/com/yevhenpiven/Operation.java
+++ Lambda/src/ua/com/yevhenpiven/Operation.java
@@ -1,5 +1,6 @@
 package ua.com.yevhenpiven;
 
 public interface Operation {
-double getResult(double value1, double value2);
+//double getResult(double value1, double value2);
+	double getResult();
 }
